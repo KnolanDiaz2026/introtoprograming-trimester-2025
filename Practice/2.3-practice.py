dae03fb40184611070316360c6c33d3214e7a374
Diaz Knolan <834947@stma.k12.mn.us>
2025-09-25 13:15:12 -0500

    messege

---
+++ Practice/2.3-practice.py
@@ -0,0 +1,16 @@
+#string-in quotations"""
+#integer-a whole number
+#float -a decimal number
+#charecter -a single glyph  "c"
+#boolagin_ true or false
+
+# how to define a variable 
+# name = value
+
+print(421)
+print(2+2)
+print(18459478567+9457348)
+x=10
+y=50
+potato=7
+print(x*y*potato)
