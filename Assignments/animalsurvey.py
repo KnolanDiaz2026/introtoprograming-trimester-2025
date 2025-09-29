512534c965b7a202c438cd2771fe738be317a62e
Diaz Knolan <834947@stma.k12.mn.us>
2025-09-29 13:15:39 -0500

    animal comments

--- Assignments/animalsurvey.py
+++ Assignments/animalsurvey.py
@@ -2,6 +2,7 @@ favorite_animal = input("What is your favorite animal?")
 why_like = input("Why do you like them?")
 where_live = input("Where do they live?")
 fun_fact = input("What is a fun fact about them?")
-color = input("what color are they?")
+color = input("what color are they?")#what these are asking in the survey
 
 print("Your favorite animal is the " + favorite_animal + " because " + why_like + ". They live in " + where_live + ", and " + fun_fact + ". They are " + color)
+#asking the questions in a sequense
