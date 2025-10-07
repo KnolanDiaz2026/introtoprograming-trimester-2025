8b6509444ba348bed0cafe8de7771801a162ffd5
Diaz Knolan <834947@stma.k12.mn.us>
2025-10-07 13:55:10 -0500

    boolean quiz

---
+++ Assignments/boolean quiz.py
@@ -0,0 +1,15 @@
+
+answer1 = int(input("Give a whole number that is less than 5."))
+print(answer1 < 5)
+
+answer2 = int(input("Give a whole number that is less than 6."))
+print(answer2 < 6)
+
+answer3 = int(input("Give a whole number that is less than 41."))
+print(answer3 < 51)
+
+answer4 = int(input("Give a whole number that is less than 69."))
+print(answer4 < 69)
+
+answer5 = int(input("Give a whole number that is less than 76."))
+print(answer5 < 76)
